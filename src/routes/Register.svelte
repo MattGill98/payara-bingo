a220a96eec77ebd9993879ab99ff1c186837ff35
Matthew Gill <matthew.gill@live.co.uk>
2020-04-30 14:42:58 +0100

    Add password confirmation
    Registration page requires that you confirm your password.
    
    Signed-off-by: Matthew Gill <matthew.gill@live.co.uk>

--- src/routes/Register.svelte
+++ src/routes/Register.svelte
@@ -6,20 +6,26 @@
 
     let email;
     let password;
+    let confirmpassword;
 
     function register() {
-        auth.createUserWithEmailAndPassword(email, password)
-            .then(userToken => {
-                navigate('/', true);
-            })
-            .catch(error => {
-                console.error('Error creating user: ' + error);
-            });
+        if (password === confirmpassword) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .then(userToken => {
+                    navigate('/', true);
+                })
+                .catch(error => {
+                    console.error('Error creating user: ' + error);
+                });
+        } else {
+            throw new Error("passwords don't match");
+        }
     }
 </script>
 
 <form on:submit|preventDefault="{register}">
-    <input type="email" bind:value="{email}" />
-    <input type="password" bind:value="{password}" />
+    <input type="email" bind:value="{email}" placeholder="Email" />
+    <input type="password" bind:value="{password}" placeholder="Password" />
+    <input type="password" bind:value="{confirmpassword}" placeholder="Confirm Password" />
     <input type="submit" value="Register" />
 </form>
